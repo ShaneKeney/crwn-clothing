070271d70aa165a87d38a95671e7791bd4d06577
Shane Keney <sckeney@gmail.com>
2022-07-28 10:57:53 -0400

    feat: firebase config

---
+++ src/utils/firebase/firebase.utils.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvrw_iGno11J0Ogj_mvJnJMKrXkOhwVc0",
+  authDomain: "crwn-clothing-db-76081.firebaseapp.com",
+  projectId: "crwn-clothing-db-76081",
+  storageBucket: "crwn-clothing-db-76081.appspot.com",
+  messagingSenderId: "651974377103",
+  appId: "1:651974377103:web:ac5e2443cec0b57904fafa",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
